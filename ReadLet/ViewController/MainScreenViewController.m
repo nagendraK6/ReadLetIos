f255392b0c114a23ef4b9b5b33276581cb2c06d8
ashurely <ashu@12labs.com>
2019-05-24 14:00:24 -0700

    Changed the copy on Update subscription Page
    Changed the copy on Update subscription Page

--- ReadLet/ViewController/MainScreenViewController.m
+++ ReadLet/ViewController/MainScreenViewController.m
@@ -91,8 +91,11 @@
     
     
     
+    UIBarButtonItem *save_button = [[UIBarButtonItem alloc]initWithTitle:@"Save" style:UIBarButtonItemStylePlain target:self action:@selector(rightBtnClick)];
     UIBarButtonItem *doneButton = [[UIBarButtonItem alloc] initWithTitle:@"Back" style:(UIBarButtonItemStyleDone) target:self action:@selector(cancelButtonAction:)];
     vc.navigationItem.leftBarButtonItem = doneButton;
+    vc.navigationItem.title = @"Update subscriptions";
+    vc.navigationItem.rightBarButtonItem=save_button;
 
     
     
